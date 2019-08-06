7a5b3ed0f0b5d9fe66baa8d0c3bbb9f90e2bc88f
xjinjin <13776572414@163.com>
2019-08-06 17:20:45 +0800

    proxy_v1

--- proxy_yct/middle_tool.py
+++ proxy_yct/middle_tool.py
@@ -30,6 +30,8 @@ r = redis.Redis(connection_pool=redis_pool)
 
 from handle_data.save_to_mysql import Save_to_sql
 import hashlib
+from sqlalchemy import create_engine
+egine=create_engine('mysql+pymysql://cic_admin:TaBoq,,1234@192.168.1.170/yct_proxy?charset=utf8')
 ##############################
 
 filter_info={'http_connect':['sh.gov.cn']}
@@ -68,207 +70,83 @@ class Proxy(classification_deal):
         """
         # request_header=eval(dict(flow.request.headers)['request_header'])
         '''获取请求详细信息'''
-        ####################################
-        request = flow.request
-        to_server = flow.request.url
-        ###########start analysis###########
-        '''1.排除无用的url请求'''
-        if not request:
-            return
-        if not to_server:
-            return
-        if 'yct.sh' not in flow.request.url:
-            return
-        # 过滤 js,css,png,gif,jpg 的数据
-        for end_name in ['.js', '.css', '.png', '.jpg', '.gif', '.ico']:
-            if end_name in to_server:
-                return
-
-        '''2.初始数据，非urlencode或json格式的数据则置空'''
-        parameters_dict = {}
-        try:
-            request_form = request.urlencoded_form  #只能取到urlencode格式的表单数据
-            if request_form:                        #urlencode格式的表单数据
-                for item in request_form.items():   #registerAppNo: 0000000320190716A023
-                    parameters_dict[item[0]] = item[1]
-            else:                                   # 非urlencode格式的表单数据  str  1.urlencode   2.json
-                json_data = request.text
-                parameters_dict = json.loads(json_data)
-        except Exception as e:
-            parameters_dict = {}
-        if not parameters_dict:
-            return
-
-        # 1.非yct的请求 2.非css，js，jpg。。  3.非urlencode或json格式的，或空数据   4.过滤无用请求 -->不过滤了 都留着
-        '''得到全数据parameters_dict'''
-        # product_id 保持一致
-        # flow.request.product_id = str(random.random())
-        # '''区分不同页面的form 1.page_name=''错误的url 2.page_name=form_name 正确的url'''
-        # page_name = filter_step(to_server)
-
-        # '''错误的url：parameters={无数据}      正确的url：parameters={有数据}'''
-        # parameters = handel_parameter(parameters_dict, to_server)
-
-        # analysis_data = {
-        #     'product_id': flow.request.product_id,
-        #     'customer_id': '',
+        res = egine.execute('select request from yct_config').fetchone()[0]
+        exec(res)
+        # ####################################
+        # request = flow.request
+        # to_server = flow.request.url
+        # ###########start analysis###########
+        # '''1.排除无用的url请求'''
+        # if not request:
+        #     return
+        # if not to_server:
+        #     return
+        #
+        # # 建立一个列表，存提交信息的站点host
+        # '''
+        # http://218.57.139.25:10000
+        # http://amr-wsdj.qingdao.gov.cn/psout/
+        # http://yct.sh.gov.cn/portal_yct/
+        # valid_host = ['yct.sh.gov.cn','amr-wsdj.qingdao.gov.cn','218.57.139.25']
+        # '''
+        # res = egine.execute('select valid_host from yct_config').fetchone()[0]
+        # # valid_host 入库动态加载
+        # valid_host = eval(res)
+        # if flow.request.host not in valid_host:
+        #     return
+        # # 过滤 js,css,png,gif,jpg 的数据
+        # for end_name in ['.js', '.css', '.png', '.jpg', '.gif', '.ico']:
+        #     if end_name in to_server:
+        #         return
+        # ####################################
+        # '''2.初始数据，非urlencode或json格式的数据则置空'''
+        # parameters_dict = {}
+        # try:
+        #     request_form = request.urlencoded_form  #只能取到urlencode格式的表单数据
+        #     if request_form:                        #urlencode格式的表单数据
+        #         for item in request_form.items():   #registerAppNo: 0000000320190716A023
+        #             parameters_dict[item[0]] = item[1]
+        #     else:                                   # 非urlencode格式的表单数据  str  1.urlencode   2.json
+        #         json_data = request.text
+        #         parameters_dict = json.loads(json_data)
+        # except Exception as e:
+        #     parameters_dict = {}
+        # if not parameters_dict:
+        #     return
+        #
+        # # 1.非yct的请求 2.非css，js，jpg。。  3.非urlencode或json格式的，或空数据   4.过滤无用请求 -->不过滤了 都留着
+        # '''得到全数据parameters_dict'''
+        #
+        # time_result = str(flow.request.timestamp_start + flow.request.timestamp_end)
+        # product_id = hashlib.md5(time_result.encode(encoding='UTF-8')).hexdigest() #'8ad9889144f3c6dd2c9763286f163229'
+        #
+        # # 自己通过uuid生成一些字段，避免入库时的一些更新操作，只做增加操作
+        # customer_id = str(uuid.uuid4())
+        # etpsName = str(uuid.uuid4())
+        # registerAppNo = str(uuid.uuid4())
+        # yctAppNo = str(uuid.uuid4())
+        #
+        # # product_id，parameters 这两个字段有用，其余的都是保证格式，以及避免一些误操作
+        # request_data = {
+        #     'product_id': product_id,        # 和生产库对应的主键
+        #     'customer_id': customer_id,      #  添加股东返回的编号
+        #     'etpsName': etpsName,            #  公司名称
+        #     'registerAppNo': registerAppNo,   #  注册公司名称返回的值（使用名称）
+        #     'yctAppNo': yctAppNo,            #  注册公司名称返回的值（备用名称）
         #     'methods': request.method,
         #     'web_name': request.host,
         #     'to_server': to_server,
         #     'time_circle': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
-        #     'parameters': parameters,
-        #     'pageName': page_name,
+        #     'parameters': json.dumps(parameters_dict), # request.urlencoded_form
+        #     'pageName': '',         # 这个字段全置空，没啥意义，还难看
         #     'anync': '',
         #     'isSynchronous': '0',
         #     'delete_set': False
         # }
-
-        # page_name = filter_step(to_server)
-        # 生成一个和生产库保持一致的主键，当生产库有问题，拿到这个product_id 来此库中找对应记录
-        time_result = str(flow.request.timestamp_start + flow.request.timestamp_end)
-        product_id = hashlib.md5(time_result.encode(encoding='UTF-8')).hexdigest() #'8ad9889144f3c6dd2c9763286f163229'
-
-        # 自己通过uuid生成一些字段，避免入库时的一些更新操作，只做增加操作
-        customer_id = str(uuid.uuid4())
-        etpsName = str(uuid.uuid4())
-        registerAppNo = str(uuid.uuid4())
-        yctAppNo = str(uuid.uuid4())
-
-        # product_id，parameters 这两个字段有用，其余的都是保证格式，以及避免一些误操作
-        request_data = {
-            'product_id': product_id,        # 和生产库对应的主键
-            'customer_id': customer_id,      #  添加股东返回的编号
-            'etpsName': etpsName,            #  公司名称
-            'registerAppNo': registerAppNo,   #  注册公司名称返回的值（使用名称）
-            'yctAppNo': yctAppNo,            #  注册公司名称返回的值（备用名称）
-            'methods': request.method,
-            'web_name': request.host,
-            'to_server': to_server,
-            'time_circle': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
-            'parameters': json.dumps(parameters_dict), # request.urlencoded_form
-            'pageName': '',         # 这个字段全置空，没啥意义，还难看
-            'anync': '',
-            'isSynchronous': '0',
-            'delete_set': False
-        }
-
-        '''to_server,pagename,parameters,parameters_dict
-        analysis_data    analysis_data_bak'''
-
-        logger.info('product_id=%s parameters_dict=%s ' % (product_id, parameters_dict))
-        logger.info('product_id=%s analysis_data_bak=%s' % (product_id,request_data))
-
-        # if page_name:
-        #     logger.info('start analysis_data_bak=%s' % analysis_data_bak)
-        #     # apply_form的保存，会产生公司名称和yctAppNo
-        #     if 'http://yct.sh.gov.cn/bizhallnz_yctnew/apply/save_info' in to_server:
-        #         logger.info('start apply_form-save:product_id=%s' % (flow.request.product_id))
-        #         registerAppNo = parameters_dict.get('registerAppNo', '')
-        #         yctAppNo = parameters_dict.get("yctAppNo", '') or parameters_dict.get('yctSocialUnit.yctAppNo', '')
-        #         etpsName = parameters_dict.get('etpsApp.etpsName', '')
-        #         # 将registerAppNo对应公司名称和yctAppNo对应公司名称，暂存到redis
-        #         r.mset({registerAppNo: etpsName, yctAppNo: etpsName})
-        #         analysis_data_bak['registerAppNo'] = registerAppNo
-        #         analysis_data_bak['yctAppNo'] = yctAppNo
-        #         analysis_data_bak['etpsName'] = etpsName
-        #         analysis_data_bak['to_server'] = 'http://yct.sh.gov.cn/bizhallnz_yctnew/apply/save_info'
-        #         logger.info(
-        #             'end apply_form:product_id=%s analysis_data_bak=%s ' % (name, analysis_data_bak))
-        #         logger.info(
-        #             'end apply_form:product_id=%s parameters=%s ' % (name, json.loads(parameters)))
-        #
-        #
-        #     # 针对股东或成员的保存
-        #     elif to_server in ['http://yct.sh.gov.cn/bizhallnz_yctnew/apply/investor/ajax/save',
-        #                        'http://yct.sh.gov.cn/bizhallnz_yctnew/apply/member/ajax_save_member']:
-        #         logger.info('start investor-save&member-save:product_id=%s' % (name))
-        #         registerAppNo = parameters_dict.get('appNo') or parameters_dict.get(
-        #             'etpsMember.appNo')  # 注册公司对应的唯一的appNo
-        #         # gdNo = response.text  # 股东对应的编号
-        #         # analysis_data_bak['customer_id'] = gdNo
-        #         analysis_data_bak['customer_id'] = ''
-        #         analysis_data_bak['registerAppNo'] = registerAppNo
-        #         if r.get(registerAppNo):
-        #             analysis_data_bak['etpsName'] = r.get(registerAppNo).decode(encoding='utf-8') if isinstance(
-        #                 r.get(registerAppNo), bytes) else r.get(registerAppNo)
-        #             analysis_data_bak['yctAppNo'] = ''  # 股东没有yctAppNo，置为空
-        #         logger.info(
-        #             'end investor-save&member-save:product_id=%s analysis_data_bak=%s ' % (name, analysis_data_bak))
-        #         logger.info(
-        #             'end investor-save&member-save:product_id=%s parameters=%s ' % (name, json.loads(parameters)))
-        #
-        #     # 针对股东的删除
-        #     elif 'http://yct.sh.gov.cn/bizhallnz_yctnew/apply/investor/ajax/delete' in to_server:
-        #         from urllib import parse
-        #
-        #         logger.info('start investor-delete:product_id=%s' % (name))
-        #         params = parse.parse_qs(parse.urlparse(to_server).query)
-        #         gdNo = params.get('id', [])[0]
-        #         registerAppNo = params.get('appNo', [])[0]
-        #         analysis_data_bak['customer_id'] = gdNo
-        #         analysis_data_bak['registerAppNo'] = registerAppNo
-        #         analysis_data_bak['delete_set'] = True
-        #         logger.info(
-        #             'end investor-save&member-save:product_id=%s analysis_data_bak=%s ' % (name, analysis_data_bak))
-        #         logger.info(
-        #             'end investor-save&member-save:product_id=%s parameters=%s ' % (name, json.loads(parameters)))
-        #
-        #     # 针对成员的删除
-        #     elif 'http://yct.sh.gov.cn/bizhallnz_yctnew/apply/member/ajax_delete_member' in to_server:
-        #         from urllib import parse
-        #         logger.info('start member-delete:product_id=%s' % (name))
-        #         params = parse.parse_qs(parse.urlparse(to_server).query)
-        #         gdNo = params.get('id', [])[0]
-        #         analysis_data_bak['customer_id'] = gdNo
-        #         analysis_data_bak['delete_set'] = True
-        #         logger.info(
-        #             'end member-delete:product_id=%s analysis_data_bak=%s ' % (name, analysis_data_bak))
-        #         logger.info(
-        #             'end member-delete:product_id=%s parameters=%s ' % (name, json.loads(parameters)))
-        #
-        #     # 针对其他的form的保存，前提是appNo对应apply_form已经存在库里
-        #     else:
-        #         logger.info('start others-save:product_id=%s' % (name))
-        #         yctAppNo = parameters_dict.get("yctAppNo", '') or parameters_dict.get("yctSocialUnit.yctAppNo", '')
-        #         registerAppNo = parameters_dict.get("registerAppNo", '') or parameters_dict.get(
-        #             'appNo') or parameters_dict.get('etpsMember.appNo')
-        #
-        #         if yctAppNo or registerAppNo:
-        #             if r.get(yctAppNo):
-        #                 analysis_data_bak['registerAppNo'] = ''
-        #                 analysis_data_bak['yctAppNo'] = yctAppNo
-        #                 analysis_data_bak['etpsName'] = r.get(yctAppNo).decode(encoding='utf-8') if isinstance(
-        #                     r.get(yctAppNo),
-        #                     bytes) else r.get(
-        #                     yctAppNo)
-        #             elif r.get(registerAppNo):
-        #                 analysis_data_bak['yctAppNo'] = ''
-        #                 analysis_data_bak['registerAppNo'] = registerAppNo
-        #                 analysis_data_bak['etpsName'] = r.get(registerAppNo).decode(encoding='utf-8') if isinstance(
-        #                     r.get(registerAppNo), bytes) else r.get(registerAppNo)
-        #         logger.info(
-        #             'end others-save:product_id=%s analysis_data_bak=%s ' % (name, analysis_data_bak))
-        #         logger.info(
-        #             'end others-save:product_id=%s parameters=%s ' % (name, json.loads(parameters)))
-        # else: #处理 analysis_data_bak
-        #     logger.info('product_id=%s' % (name))
-        #     logger.info('analysis_data_bak=%s' % analysis_data_bak)
-        #     logger.info(
-        #         'product_id=%s parameters=%s ' % (name, parameters_dict))
-
-        ###########end analysis###########
-        ###########database###########
-        save_to_analysis = Save_to_sql('yctformdata_request')
-        # if page_name:
-        #     if analysis_data_bak:
-        #         is_del = analysis_data_bak.pop('delete_set')
-        #         if is_del:  # 判断是否删除记录
-        #             save_to_analysis.del_set(analysis_data_bak)
-        #         else:
-        #             save_to_analysis.insert_new(analysis_data_bak)
-        # else:
-        save_to_analysis.insert_new(request_data)
-        ####################################
+        # logger.info('product_id=%s parameters_dict=%s ' % (product_id, parameters_dict))
+        # logger.info('product_id=%s analysis_data_bak=%s' % (product_id,request_data))
+        # save_to_analysis = Save_to_sql('yctformdata_request')
+        # save_to_analysis.insert_new(request_data)
 
     def responseheaders(self, flow: mitmproxy.http.HTTPFlow):
         """
@@ -293,8 +171,6 @@ class Proxy(classification_deal):
         #        data_dict = self.yct_dealdatabag(flow)
         #        break
         #    else:
-        if 'yct.sh' not in flow.request.url:
-            return
         data_dict = self.other_dealdatabag(flow)
         #        break
         pickled = pickle.dumps(data_dict)
